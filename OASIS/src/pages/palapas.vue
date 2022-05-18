fe444b91765b2c8398fe8cc388760c942e434f2d
Jaime Mendez <jaimemendez04@gmail.com>
2022-05-18 08:49:28 -0600

    First commit

---
+++ OASIS/src/pages/palapas.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="RESIDENCIAL OASIS"
+      src="/images/quasar-logo-vertical.svg"
+    >
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
+
+<style>
+
+</style>
